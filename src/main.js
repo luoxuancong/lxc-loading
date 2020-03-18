3dd9eea46da7db428c2b3df8453aaf5fdc61faec
luoxuancong <742340151@qq.com>
2020-03-18 11:31:15 +0800

    克隆代码  安装依赖 可以预览插件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App'
+import LLoading from './lib/index'
+Vue.use(LLoading)
 new Vue({
   el: '#app',
   render: h => h(App)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 var path = require('path')
 var webpack = require('webpack')
 module.exports = {
-  // entry: './src/main.js',
-  entry: './src/lib/index.js',// 改变入口
+  entry: './src/main.js', // 开发
+  // entry: './src/lib/index.js',// npm发布 改变入口
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: "lxcLoading.js", // 修改名称
-    library: "lxcLoading", // 指定的就是你使用require时的模块名
-    libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
-    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
+    filename: 'build.js', // 开发
+    /** npm 发布  **/
+    // filename: "lxcLoading.js", // 修改名称
+    // library: "lxcLoading", // 指定的就是你使用require时的模块名
+    // libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+    // umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
   module: {
     rules: [
